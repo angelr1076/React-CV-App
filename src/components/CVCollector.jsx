353fe30cc96faaa5a80c67190ab4b566fee5dc2f
angelr1076 <angelr1076@gmail.com>
2023-10-30 11:46:35 -0400

    Added Accordian component from chakraui to CVCollector component for the Education and Work forms' duplication. Added react-icons

--- src/components/CVCollector.jsx
+++ src/components/CVCollector.jsx
@@ -3,7 +3,18 @@ import { GeneralInfoForm } from './GeneralInfoForm';
 import { EducationForm } from './EducationForm';
 import { WorkExperienceForm } from './WorkExperienceForm';
 import { CVContext } from './CVProvider';
-import { Heading, Text, Button } from '@chakra-ui/react';
+import {
+  Heading,
+  Text,
+  Button,
+  Box,
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+} from '@chakra-ui/react';
+import { GrAddCircle } from 'react-icons/gr';
 
 function CVCollector() {
   const { addGeneralInfo, addEducation, addWorkExperience } =
@@ -30,22 +41,39 @@ function CVCollector() {
         <Heading as='h3' size='md' noOfLines={1} mb={5}>
           <Text fontSize='2xl'>Education</Text>
         </Heading>
-        {educationForms.map((number, index) => (
-          <EducationForm key={index} addEducation={addEducation} />
+        {/* Education Forms */}
+        {educationForms.map((item, index) => (
+          <Accordion key={index} allowMultiple>
+            <AccordionItem>
+              <h2>
+                <AccordionButton>
+                  <Box as='span' flex='1' textAlign='left'>
+                    Education ({index + 1})
+                  </Box>
+                  <AccordionIcon />
+                </AccordionButton>
+              </h2>
+              <AccordionPanel pb={4}>
+                <EducationForm key={index} addEducation={addEducation} />
+              </AccordionPanel>
+            </AccordionItem>
+          </Accordion>
         ))}
         <Button
           onClick={handleAddEducationForm}
-          variant='outline'
+          leftIcon={<GrAddCircle />}
           colorScheme='teal'
+          variant='ghost'
           mt={2}>
-          + Education
+          Education
         </Button>
       </div>
       <div>
+        {/* Work forms */}
         <Heading as='h3' size='md' noOfLines={1} mb={5}>
           <Text fontSize='2xl'>Work Experience</Text>
         </Heading>
-        {workForms.map((number, index) => (
+        {workForms.map((item, index) => (
           <WorkExperienceForm
             key={index}
             addWorkExperience={addWorkExperience}
@@ -53,10 +81,11 @@ function CVCollector() {
         ))}
         <Button
           onClick={handleAddWorkForm}
-          variant='outline'
+          leftIcon={<GrAddCircle />}
           colorScheme='teal'
+          variant='ghost'
           mt={2}>
-          + Work
+          Work
         </Button>
       </div>
     </section>
