5d0ad0bffb069b40a155fc4767433485023fdbfa
angelr1076 <angelr1076@gmail.com>
2023-11-08 07:26:33 -0500

    Added a chakraui divider between the form-container and display-container to create separation and added styling attributes to each Box container.

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,28 @@ import './assets/styles/style.css';
 import { CVProvider } from './contexts/CVContext';
 import { CVForm } from './components/CVForm';
 import { CVDisplay } from './components/CVDisplay';
-import { Box } from '@chakra-ui/react';
+import { Box, Divider } from '@chakra-ui/react';
 
 function App() {
   return (
     <CVProvider>
       <Box className='main' border='1px' borderColor='gray.300' display='flex'>
-        <Box flex='1' className='form-container'>
+        <Box
+          flex='1'
+          boxShadow='lg'
+          p={4}
+          bg='white'
+          className='form-container'>
           <CVForm />
         </Box>
-        <Box flex='3' className='display-container'>
+        <Divider orientation='vertical' />
+        <Box
+          flex='3'
+          boxShadow='lg'
+          p={4}
+          bg='gray.50'
+          marginLeft={4}
+          className='display-container'>
           <CVDisplay />
         </Box>
       </Box>
