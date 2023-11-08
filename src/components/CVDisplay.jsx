c07ee70394bad8d3fadfac14e175abb861c71669
angelr1076 <angelr1076@gmail.com>
2023-11-08 16:13:54 -0500

    Added all styling to CVDisplay to display resume

--- src/components/CVDisplay.jsx
+++ src/components/CVDisplay.jsx
@@ -1,6 +1,6 @@
 import { useContext } from 'react';
 import { CVContext } from '../contexts/CVContext';
-import { Box, Text, VStack, HStack, Icon } from '@chakra-ui/react';
+import { Box, Text, Flex, VStack, HStack, Icon } from '@chakra-ui/react';
 import {
   MdOutlineMailOutline,
   MdOutlinePhonelinkRing,
@@ -58,55 +58,84 @@ const CVDisplay = () => {
           </Text>
         )}
         {education?.map(edu => (
-          <Box key={edu.id} className='education-entry' mb={4}>
-            <Text className='school-name' fontSize='sm'>
-              {edu.schoolName}
+          <Flex
+            key={edu.id}
+            className='education-entry'
+            mb={4}
+            alignItems='center'
+            justifyContent='space-between'>
+            <VStack alignItems='flex-start' ml={4}>
+              <Text className='school-name' fontSize='sm' fontWeight='bold'>
+                {edu.schoolName}
+              </Text>
+              <HStack>
+                <Text className='date-from' fontSize='sm'>
+                  {edu.dateFrom} {edu.dateFrom && edu.dateTo && ' - '}
+                </Text>
+                <Text className='date-to' fontSize='sm'>
+                  {edu.dateTo}
+                </Text>
+              </HStack>
+            </VStack>
+            <Text
+              className='major'
+              fontSize='sm'
+              alignItems='flex-start'
+              mr={10}>
+              {edu.major && <Text as='b'>Major: </Text>} {edu.major}
             </Text>
-            <Text className='major' fontSize='sm'>
-              {edu.major}
-            </Text>
-            <Text className='date-from' fontSize='sm'>
-              {edu.dateFrom}
-            </Text>
-            <Text className='date-to' fontSize='sm'>
-              {edu.dateTo}
-            </Text>
-          </Box>
+          </Flex>
         ))}
       </Box>
 
       {/* Experience Section */}
-      <Box className='experience-section' p={4} borderRadius='md' mb={6}>
-        {experience && experience.length > 0 && experience[0].companyName && (
+      <VStack
+        className='experience-section'
+        p={4}
+        borderRadius='md'
+        mb={6}
+        align='stretch'>
+        {experience && experience.length > 0 && (
           <Text
             className='section-title'
             bg='gray.300'
             color='black'
             fontSize='xl'
-            mb={1}>
+            mb={4}>
             Experience
           </Text>
         )}
         {experience?.map(exp => (
-          <Box key={exp.id} className='experience-entry' mb={4}>
-            <Text className='company-name' fontSize='sm'>
-              {exp.companyName}
-            </Text>
-            <Text className='position-title' fontSize='sm'>
-              {exp.positionTitle}
-            </Text>
-            <Text className='responsibilities' fontSize='sm'>
-              {exp.responsibilities}
-            </Text>
-            <Text className='date-from' fontSize='sm'>
-              {exp.dateFrom}
-            </Text>
-            <Text className='date-to' fontSize='sm'>
-              {exp.dateTo}
-            </Text>
-          </Box>
+          <Flex
+            key={exp.id}
+            className='experience-entry'
+            mb={4}
+            justify='space-between'
+            alignItems='flex-start'>
+            <VStack align='flex-start'>
+              <Text className='date-from' fontSize='sm'>
+                {exp.dateFrom}
+              </Text>
+              <Text className='date-to' fontSize='sm'>
+                {exp.dateTo}
+              </Text>
+            </VStack>
+            <VStack align='flex-end'>
+              <Text className='company-name' fontSize='sm' fontWeight='bold'>
+                {exp.companyName}
+              </Text>
+              <Text className='position-title' fontSize='sm' as='i'>
+                {exp.positionTitle}
+              </Text>
+            </VStack>
+            <Box flex='1' pl={4}>
+              <Text className='responsibilities' fontSize='sm'>
+                {exp.responsibilities}
+              </Text>
+            </Box>
+          </Flex>
         ))}
-      </Box>
+      </VStack>
     </Box>
   );
 };
